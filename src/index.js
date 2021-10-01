f26e5c744127d0d556e9e58c561061b0b7247a6d
steven-huaa <88639298+steven-huaa@users.noreply.github.com>
2021-10-01 19:54:24 +1000

    added boxes

--- src/index.js
+++ src/index.js
@@ -17,6 +17,42 @@ const App = () => {
     return () => window.removeEventListener("resize", onResize);
   }, []);
 
+  const idata = [
+    {
+      id: "BRa2xX",
+      mark: {
+        x: 84.393063583815,
+        y: 107.51445086705202,
+        width: 211.56069364161849,
+        height: 83.2369942196532,
+        type: "RECT"
+      },
+      comment: "Google"
+    },
+    {
+      id: "QtPJeW",
+      mark: {
+        x: 469.3641618497109,
+        y: 86.70520231213872,
+        width: 221.96531791907518,
+        height: 149.1329479768786,
+        type: "RECT"
+      },
+      comment: "Mozilla"
+    },
+    {
+      id: "czysBh",
+      mark: {
+        x: 153.757225433526,
+        y: 322.5433526011561,
+        width: 201.15606936416185,
+        height: 83.23699421965318,
+        type: "RECT"
+      },
+      comment: "IPsoft"
+    }
+  ];
+
   const onSelect = (selectedId) => {};
   const onChange = (data) => {};
 
@@ -28,6 +64,7 @@ const App = () => {
         onChange={onChange}
         width={pageSize.width}
         height={pageSize.height}
+        annotationData={idata}
       />
     </div>
   );
